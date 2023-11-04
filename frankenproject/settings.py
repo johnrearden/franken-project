0f4b8259c29f39ead801e310772efc6711becb5d
johnrearden <isnocturnal@gmail.com>
2023-11-04 20:03:33 +0000

    Experiment with CLOUNDINARY_STORAGE settings

--- frankenproject/settings.py
+++ frankenproject/settings.py
@@ -162,6 +162,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 
+# CLOUDINARY CONFIGURATION
+CLOUDINARY_STORAGE = {
+    MEDIA_TAG: "frankenproject_static",
+    STATIC_TAG: "frankenproject_static"
+}
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
