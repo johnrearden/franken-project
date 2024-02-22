8f6db30e435532b543b99456c20e7ff8364ff6a5
johnrearden <isnocturnal@gmail.com>
2024-02-22 10:37:35 +0000

    Add env.example.py

---
+++ env.example.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ.setdefault('SECRET_KEY', 'A;LSKDJFALKJSDLFJALKSDJFLKALJ')
+os.environ.setdefault('DATABASE_URL', 'postgres://uyut4etsbv2:qwnqjGwC3x3S@ep-gentle-mountain-a23bxz6h.eu-central-1.aws.neon.tech/sugar_curvy_skies_120800')
+os.environ.setdefault('CLOUDINARY_URL', 'cloudinary://369292388473571:WorVa4KALvMT9ROPMxzBLWt3cXQ@dcgbp2n9p')
