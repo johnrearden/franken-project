e40a3001d03b48e3266c0b70cda69fb45bc6bfa5
John Rearden <isnocturnal@gmail.com>
2024-11-17 17:48:08 +0000

    Correct missing entries in env.example.py

--- env.example.py
+++ env.example.py
@@ -3,3 +3,5 @@ import os
 os.environ.setdefault('SECRET_KEY', '')
 os.environ.setdefault('DATABASE_URL', '')
 os.environ.setdefault('CLOUDINARY_URL', '')
+os.environ.setdefault('ALLOWED_HOSTS', '127.0.0.1')
+os.environ.setdefault('DEBUG', '1')
